db155fc3e43862da923bc7bfd8969dab95c0f726
Sergey Shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-11-13 20:18:02 +0300

    Third practic

--- src/test/java/LogsTest.java
+++ src/test/java/LogsTest.java
@@ -1,20 +1,21 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.Logs;
 
-public class SampleTest {
+public class LogsTest {
+
     protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(SampleTest.class);
+    private Logger logger = LogManager.getLogger(LogsTest.class);
 
     // Чтение передаваемого параметра browser (-Dbrowser)
     String env = System.getProperty("browser", "chrome");
-
-
 
     @BeforeEach
     public void setUp() {
@@ -24,9 +25,25 @@ public class SampleTest {
     }
 
     @Test
-    public void openPage() {
+    public void logsTest() {
         driver.get("https://www.dns-shop.ru/");
         logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
+
+        // Вывод логов браузера
+        logger.info("Логи браузера");
+        Logs logs = driver.manage().logs();
+        LogEntries logEntries = logs.get(LogType.BROWSER);
+        for (LogEntry logEntry : logEntries) {
+            logger.info(String.format("%s", logEntry.getMessage()));
+        }
+        logger.info("--------------------------------------");
+
+        // Добавление задержки Thread.sleep, чтобы увидеть результат
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
--- src/test/java/TimeOutsTest.java
+++ src/test/java/TimeOutsTest.java
@@ -1,20 +1,19 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SampleTest {
+import java.util.concurrent.TimeUnit;
+
+public class TimeOutsTest {
+
     protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(SampleTest.class);
+    private Logger logger = LogManager.getLogger(TimeOutsTest.class);
 
     // Чтение передаваемого параметра browser (-Dbrowser)
     String env = System.getProperty("browser", "chrome");
-
-
 
     @BeforeEach
     public void setUp() {
@@ -24,7 +23,17 @@ public class SampleTest {
     }
 
     @Test
-    public void openPage() {
+    public void timeOutsTest1() {
+        // Ожидание загрузки страницы в 60 секунд
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.get("https://www.dns-shop.ru/");
+        logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
+    }
+
+    @Test
+    public void timeOutsTest2() {
+        // Ожидание загрузки страницы в 1 миллисекунду
+        driver.manage().timeouts().pageLoadTimeout(1, TimeUnit.MILLISECONDS);
         driver.get("https://www.dns-shop.ru/");
         logger.info("Открыта страница DNS - " + "https://www.dns-shop.ru/");
     }
